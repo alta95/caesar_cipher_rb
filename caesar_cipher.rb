9831eb1ddcc0d0bcf92561413ca043f36c7968f0
alta95 <fajarridalta@gmail.com>
2021-06-24 18:36:36 +0700

    Done, not as function

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,33 +1,40 @@
-# text = ''
-# direction = ''
-# shift = ''
+#sample: ABCD abcd 1234 1A2b3C4d....
 
-# loop do
-#     puts 'Text to encrypt:'
-#     text = gets.chomp
-#     break if text!=''
-# end
-
-# loop do
-#     puts 'left/right?'
-#     direction = gets.chomp
-#     break if (direction =='left' || direction =='right')
-# end
-
-# loop do
-#     puts 'Number of shift:'
-#     shift = gets.chomp
-#     break if shift.to_i != 0
-# end
-
-# puts "Encrypt:'" + text + "', with a " + direction + " shift of " + shift 
-
-text = 'What a string!'
-direction = 'left'
-shift = '5'
+text = ''
+direction = ''
+shift = ''
 result = ''
-qtyShift = 5
+qtyShift = ''
 letterCase = ''
+
+loop do
+    puts 'Text to encrypt:'
+    text = gets.chomp
+    break if text!=''
+end
+
+loop do
+    puts 'left/right?'
+    direction = gets.chomp
+    break if (direction =='left' || direction =='right')
+end
+
+loop do
+    puts 'Number of shift:'
+    shift = gets.chomp
+    break if shift.to_i != 0
+end
+
+puts "Encrypt:'" + text + "', with a " + direction + " shift of " + shift 
+
+if direction == 'left'
+    qtyShift = shift.to_i*-1
+    puts qtyShift
+else 
+    qtyShift = shift.to_i
+    puts qtyShift
+end
+
 
 text.each_byte do |byte|
 
@@ -42,20 +49,15 @@ text.each_byte do |byte|
 
     if byte.between?(65, 90)  || byte.between?(97, 122)
         byte +=  qtyShift
-        #check if byte.between?(65, 90)  || byte.between?(97, 122)
-        #upper & <65
         while byte < 65 && letterCase == 'upper' do
             byte += 26
         end
-        #upper & >90
         while byte > 90 && letterCase == 'upper' do
             byte -= 26
         end
-        #lower & <97
         while byte < 97 && letterCase == 'lower' do
             byte += 26
         end
-        #lower & > 122
         while byte >122 && letterCase == 'lower' do
             byte -= 26
         end
