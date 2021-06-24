70dc9d4bcbe7502a2ed606c6c585ec1f985fa2ae
alta95 <fajarridalta@gmail.com>
2021-06-24 17:32:45 +0700

    Input Validation

---
+++ caesar_cipher.rb
@@ -0,0 +1,24 @@
+# text = ''
+# direction = ''
+# shift = ''
+
+# loop do
+#     puts 'Text to encrypt:'
+#     text = gets.chomp
+#     break if text!=''
+# end
+
+# loop do
+#     puts 'left/right?'
+#     direction = gets.chomp
+#     break if (direction =='left' || direction =='right')
+# end
+
+# loop do
+#     puts 'Number of shift:'
+#     shift = gets.chomp
+#     break if shift.to_i != 0
+# end
+
+# puts "Encrypt:'" + text + "', with a " + direction + " shift of " + shift 
+
